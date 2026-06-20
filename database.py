05d9136d7239cfd3bbc60988ea7ee5023a0fddca
ficserbiYY <ficserbiy@gmail.com>
2026-06-20 12:30:20 +0500

    refactor: set up database connection

---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlmodel import SQLModel
+from config import settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+import redis.asyncio as redis
+from typing import Final
+
+engine: Final = create_async_engine(settings.DATABASE_URL, echo=True)
+redis_client: Final = redis.from_url(settings.REDIS_URL, decode_responses=True)
+
+
+async_session_factory: Final = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False
+)
+
+async def get_session():
+    async with async_session_factory() as session:
+        yield session
+           
+        
+async def create_db_and_tables():
+    ''' Create database tables based on SQLModel schemas. '''
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
